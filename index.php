1bc8098aec6a2ddbbd1a911976c753470f6df0a7
VošplouchTornádoCZgamingLP <102223160+HavelJakub@users.noreply.github.com>
2022-10-20 13:18:48 +0200

    header
    vytvorit slozku obrazky a do te ty obrazky dat

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Králové Vrátkova</title>
+</head>
+<header>
+    <div class="navigace">
+        <div class="nav">
+            <div class="logo-box">
+                <a href="../index.php"><img class="ssmg" src="obrazky/bile_logo.png"></a>
+            </div>
+            <div class="pravy">
+                <a href=""><img class="batoh" src="obrazky/batoh.png" alt="batoh"></a>
+                <a href=""><img class="uzivatel" src="obrazky/uzivatel.png" alt="uzivatel"></a>
+            </div>
+        </div>
+        <div class="relative">
+        <div class="content-image">
+                <img src="obrazky/tricko.png">
+            </div>
+            <div class="content">
+                <h1 class="horni">BUY,<br>
+                WEAR,<br>
+                GO</h1>
+                <div class="a_relative">
+                    <a href="">BUY NOW</a>
+                    </div>
+            </div>  
+        </div>
+            
+    
+</header>
+<body>
+    
+</body>
+</html>
