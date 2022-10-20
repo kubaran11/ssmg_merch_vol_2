1317a390fcb071c7eee0bb06813655d66a5d0c12
gabrijir11 <102228437+gabrijir11@users.noreply.github.com>
2022-10-20 13:31:50 +0200

    Add files via upload

---
+++ main +footer/product_site/produkt.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/produkt.css">
+    <title>Produkt</title>
+</head>
+<body>
+<div class="container">
+    <div class="produkt">
+        <h1>Produkt</h1>
+        <img src="../images/placka.png" alt="placka">
+    </div>
+    <div class="options">
+        <div class="images">
+            <img class="circle"></img>
+            <img class="circle"></img>
+            <img class="circle"></img>
+        </div>
+        <div class="buy-sell">
+            <div id="cena">
+                <p>cena</p>
+            </div>
+            <button id="zakoupit">
+                <p>zakoupit</p>
+            </button>
+        </div>
+    </div>
+
+</div>
+
+ <div class="text">
+
+        <p>In sem justo, commodo ut, suscipit at, pharetra vitae, orci. Aenean id metus id velit ullamcorper pulvinar. Nunc auctor.
+            <br>
+            Praesent dapibus. Aliquam id dolor. Maecenas lorem. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
+            <br>
+            Fusce nibh. Curabitur sagittis hendrerit ante. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero.
+            <br>
+            Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus,
+
+        </p>
+
+    </div>
+
+    
+</body>
+</html>
